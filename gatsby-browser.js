6b4db24be5c0b55fd65041e9b589878e2b323555
SaifurReh <92114086+SaifurReh@users.noreply.github.com>
2024-09-13 12:00:38 +0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
 import { NotificationProvider } from './src/context/AddItemNotificationProvider';
 
-export const wrapRootElement = ({ element }) => (
-  <NotificationProvider>{element}</NotificationProvider>
-);
+// Function to add AdSense script to the document head
+const addAdSenseScript = () => {
+  const script = document.createElement('script');
+  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5419128939391540";
+  script.async = true;
+  script.crossOrigin = "anonymous";
+  document.head.appendChild(script);
+};
+
+export const wrapRootElement = ({ element }) => {
+  React.useEffect(() => {
+    addAdSenseScript();
+  }, []);
+
+  return <NotificationProvider>{element}</NotificationProvider>;
+};
+
